1673bf013f2c54f23fd9b2b88bf75a61a2f113a8
Bruno <bfsmurino@gmail.com>
2018-12-18 01:36:42 +0000

    JSfun script started

--- js/jsfun.js
+++ js/jsfun.js
@@ -2,6 +2,28 @@ console.log("Hello World!")
 
 var myElement = document.querySelector(".content p")
 
-myElement.innerHTML = "Hello"
+myElement.innerHTML = ""
+myElement.id = "circle"
 
-myElement.style.cssText = "color: red;"
+myElement.style.color = "red";
+myElement.style.top = 0;
+myElement.style.left = 0;
+
+window.addEventListener("keydown", function (event) {
+    switch (event.key) {
+      case "ArrowDown":
+        // code for "down arrow" key press.
+        break;
+      case "ArrowUp":
+        // code for "up arrow" key press.
+        break;
+      case "ArrowLeft":
+        alert("Hi")
+        break;
+      case "ArrowRight":
+      myElement.style.left = 50;
+        break;
+      default:
+        return; // Quit when this doesn't handle the key event.
+    }
+  }, false);
